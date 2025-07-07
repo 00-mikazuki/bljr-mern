cae5ba11fb7407f1b527e1bae19e5976fd1b66ed
00-mikazuki <diojuan432@gmail.com>
2025-07-07 20:12:26 +0700

    commit for blog-rest-api without s3

--- blog-rest-api/controllers/file.js
+++ blog-rest-api/controllers/file.js
@@ -1,6 +1,7 @@
+const fs = require('fs');
 const path = require('path');
-const { fileValidator: { validateExtension } } = require('../validators');
-const { awsS3: { uploadFileToS3, signedUrl, deleteFileFromS3 } } = require('../utils');
+// const { fileValidator: { validateExtension } } = require('../validators');
+// const { awsS3: { uploadFileToS3, signedUrl, deleteFileFromS3 } } = require('../utils');
 const { File } = require('../models');
 
 // function to upload a file to S3 and save its metadata in the database
@@ -8,28 +9,8 @@ const uploadFile = async (req, res, next) => {
   try {
     const { file } = req;
 
-    if (!file) {
-      res.code = 400;
-      throw new Error('File not found');
-    }
-
-    const ext = path.extname(file.originalname).toLowerCase();
-    const isValidExt = validateExtension(ext);
-
-    if (!isValidExt) {
-      res.code = 400;
-      throw new Error('Invalid file type. Only JPG, JPEG, and PNG are allowed.');
-    }
-
-    const fileKey = await uploadFileToS3({ file, ext });
-
-    if (!fileKey) {
-      res.code = 500;
-      throw new Error('Error uploading file to S3');
-    }
-
     const newFile = new File({
-      key: fileKey,
+      key: file.filename,
       size: file.size,
       mimetype: file.mimetype,
       createdBy: req.user._id,
@@ -49,35 +30,47 @@ const uploadFile = async (req, res, next) => {
 }
 
 // function to get a signed URL for a file stored in S3
-const getSignedUrl = async (req, res, next) => {
-  try {
-    const { key } = req.query;
-    const url = await signedUrl(key);
+// const getSignedUrl = async (req, res, next) => {
+//   try {
+//     const { key } = req.query;
+//     const url = await signedUrl(key);
 
-    if (!url) {
-      res.code = 404;
-      throw new Error('File not found');
-    }
+//     if (!url) {
+//       res.code = 404;
+//       throw new Error('File not found');
+//     }
 
-    res.status(200).json({
-      code: 200,
-      status: true,
-      message: 'Signed URL generated successfully',
-      data: {
-        url,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-}
+//     res.status(200).json({
+//       code: 200,
+//       status: true,
+//       message: 'Signed URL generated successfully',
+//       data: {
+//         url,
+//       },
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// }
 
-// function to delete a file from S3 and remove its metadata from the database
+// function to delete a file from storage and remove its metadata from the database
 const deleteFile = async (req, res, next) => {
   try {
     const { key } = req.query;
-    await deleteFileFromS3(key);
-    await File.findOneAndDelete({ key });
+    if (!key) {
+      res.code = 400;
+      throw new Error('File key is required');
+    }
+
+    // Delete the file from storage
+    const filePath = path.join(__dirname, '../uploads', key);
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+      await File.findOneAndDelete({ key });
+    } else {
+      res.code = 404;
+      throw new Error('File not found');
+    }
 
     res.status(200).json({
       code: 200,
@@ -91,6 +84,5 @@ const deleteFile = async (req, res, next) => {
 
 module.exports = {
   uploadFile,
-  getSignedUrl,
   deleteFile,
 };
--- blog-rest-api/middlewares/upload.js
+++ blog-rest-api/middlewares/upload.js
@@ -1,8 +1,36 @@
 const multer = require('multer');
+const path = require('path');
+const { generateCode } = require('../utils');
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, './uploads');
+  },
+  filename: (req, file, callback) => {
+    const originalName = file.originalname;
+    const extension = path.extname(originalName);
+    const nameWithoutExtension = originalName.replace(extension, '');
+    const sanitizedFileName = nameWithoutExtension.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+    const now = new Date();
+    const uniqueSuffix = `${now.getFullYear()}_${now.getMonth() + 1}_${now.getDate()}_${generateCode(5)}`;
+    const newFileName = `${sanitizedFileName}_${uniqueSuffix}${extension}`;
+
+    callback(null, newFileName);
+  }
+});
 
 const upload = multer({
-  storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
+  storage,
+  fileFilter: (req, file, callback) => {
+    const fileTypes = /jpeg|jpg|png/;
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = fileTypes.test(file.mimetype);
+
+    if (mimetype && extname) {
+      return callback(null, true);
+    }
+    callback(new Error('Error: File upload only supports the following filetypes - ' + fileTypes));
+  }
 });
 
 module.exports = upload;
